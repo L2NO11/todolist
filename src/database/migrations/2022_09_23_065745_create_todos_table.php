3cd5673365bf3f568c6f2fd479e363b864cc496c
Teerapong Jangthanong <teerapong.jan@ascendcorp.com>
2022-09-23 19:34:42 +0700

    feat: src

--- src/database/migrations/2022_09_23_065745_create_todos_table.php
+++ src/database/migrations/2022_09_23_065745_create_todos_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('fname');
-            $table->string('lname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->dateTime('date');
+            $table->string('content');
+            $table->boolean('completed')->default(false);
+            $table->foreignId('user_id')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todos');
     }
 };
